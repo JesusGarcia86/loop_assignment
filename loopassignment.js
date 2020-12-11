966f6bae471bddce4086299edef5b611b2051e44
JesusGarcia86 <the_sampritas@hotmail.com>
2020-12-11 09:49:48 -0800

    first commit

---
+++ loopassignment.js
@@ -0,0 +1,13 @@
+for(var x = 1; x < 20; x += 2){
+    console.log(x)
+}
+
+1,3,5,7,9,11,13,15,17,19 
+
+var sum = 0;
+for(let i = 0; i < 6; i++){
+    sum += i
+    console.log('num:', i, 'sum:', sum)
+}
+
+num: 1, sum: 1, num: 2, sum: 3, num: 3, sum: 6, num: 4, sum: 10, num: 5, sum: 15
